753f3bb27385c4e68b4eda707b2437f2cbca097c
Ani <a.gvin3@gmail.com>
2025-07-23 17:23:56 +0400

    Feat ( Sign-up ) : add sign-up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/schema/user.schema';
+import * as bcrypt from 'bcrypt';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel('user') private readonly userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp({
+    email,
+    confrimPassword,
+    firstName,
+    lastName,
+    password,
+  }: SignUpDto) {
+    const existUser = await this.userModel.findOne({ email });
+    if (existUser) throw new BadRequestException('User already exists');
+
+    if (password !== confrimPassword) {
+      throw new BadRequestException(
+        'Password and confirm password do not match',
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await this.userModel.create({
+      email,
+      password: hashedPassword,
+      confirmPassword:password,
+      firstName,
+      lastName,
+    });
+
+    return ' user registered successfully'
+  }
+}
---
+++ src/auth/dto/sign-up.dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 20)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 20)
+  confrimPassword: string;
+}
